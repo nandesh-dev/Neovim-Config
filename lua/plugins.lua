e3b8fe3967506d3575d84dd73c6bc7190b3196f4
Nandesh <nandesh.dev@gmail.com>
2024-06-03 14:06:58 +0530

    add surround plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -268,6 +268,16 @@ local plugins = {
       require('tailwind-fold').setup({ft = {'html', 'jsx'}})
     end
   },
+  {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  }
 }
 
 return plugins
